acc73644f536d45e39f844c4f2207abef54c7f3d
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-14 09:03:15 +0500

    adding provider to our index.js (Parent)

--- src/store/store.js
+++ src/store/store.js
@@ -2,5 +2,4 @@ import { createStore } from "redux";
 import { reducers } from "./reducers";
 
 // Creating a store that will keep all of our reducers
-
-
+export const store = createStore(reducers, {});
